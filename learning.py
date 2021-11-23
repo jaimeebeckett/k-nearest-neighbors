453f619a0f1d957d37ab286602174e768f69e872
jaimeebeckett <jaimeebeckett15@gmail.com>
2021-11-22 22:19:11 -0500

    refactored

--- learning.py
+++ learning.py
@@ -1,130 +1,100 @@
 import collections
-import time
-from timeit import default_timer as timer
-import numpy as np
 import pandas as pd
-from math import sqrt, floor
-from evaluation import eval_metric
+import numpy as np
+from math import fabs
 
 
-def k_fold(df, label, k=5, categorical=True, validation=False):
+def euclidean_distance(train, test):
     """
-    :param df: DataFrame to fold
-    :param label: name of column containing correct values
-    :param k: int, number of folds
-    :param categorical: boolean, True if categorical, False if regression
-    :param validation: boolean, True if taking out a validation set
-    :return: a dictionary of the folds and the validation DataFrame
+    Finds all of the euclidean distances between a data point and a DataFrame of data points
+    :param train: A DataFrame
+    :param test: A DataFrame
+    :return: A pandas Series of euclidean distances
     """
-    folds = {}
-    valid_data = pd.DataFrame()
-    valid_fold_size = 0
-    if categorical:  # if we need to stratify y
-        df = df.sort_values(label)
-    for i in range(k):
-        if validation:
-            valid_fold_size = floor(len(df) * 0.20)//k
-            valid_data = valid_data.append(df[df.index % k == i][:valid_fold_size - 1])
-        folds[i] = df[df.index % k == i][valid_fold_size:]
-    return folds, valid_data
+    broadcast = train.to_numpy()[:, np.newaxis] - test.to_numpy()
+    distance = np.abs(np.sum(broadcast**2, axis=2))
+    return pd.DataFrame(distance).set_index(train.index)
 
 
-def k_nearest_neighbors(train, test, label, classification=True, k=1):
-    """
-    :param train: The training DataFrame
-    :param test: The test DataFrame
-    :param label: The name of the column containing the correct values
-    :param classification: Boolean indicating if the data is classification or regression
-    :param k: The number of neighbors
-    :return: A list of the predicted values
-    """
-    predicted = []
-    train_no_labels = train.drop(columns=label)
-    test_no_labels = test.drop(columns=label)
-    for i in range(len(test)):
-        distance = euclidean_distance(train_no_labels, test_no_labels.iloc[i])
-        k_neighbors = distance.nsmallest(k)
-        neighbor_classes = train.loc[k_neighbors.index]
-        if classification:
-            predicted.append(find_class(neighbor_classes, label))
+def radial_basis_fn_gaussian_kernel(self, distance, sigma):
+    gaussian_kernel = np.exp(-(distance**2) / (2*sigma ** 2))
+    return gaussian_kernel
+
+
+class KNearestNeighbor:
+    def __init__(self, target, k=1, sigma=None):
+        self.k = k
+        self.target = target
+        self.sigma = sigma
+        self.predictions = None
+
+    def calculate_knn(self, train, train_label, test):
+        distance = euclidean_distance(train, test)
+        if self.sigma:
+            distance = radial_basis_fn_gaussian_kernel(distance, self.sigma)
+        k_neighbors = np.argsort(distance, axis=0)[:self.k]
+        self.predict_class(train_label.to_list(), k_neighbors)
+
+    def predict_class(self, label, k_neighbors):
+        neighbors = pd.DataFrame(k_neighbors).replace(dict(enumerate(label)))
+        if self.sigma:
+            self.predictions = neighbors.mean().values
         else:
-            print(neighbor_classes)
-            predicted.append(neighbor_classes[label].mean())
-    #distance = test_no_labels.apply(lambda x: euclidean_distance(x, train_no_labels), axis=1)
-    #k_neighbor_classes = {}
-    #for col in distance.T:
-    #    classes = train['class'].loc[distance.T[col].nsmallest(k).index].to_list()
-    #    most_frequent_class = collections.Counter(classes).most_common()[0][0]
-    #    k_neighbor_classes[col] = most_frequent_class
-    return predicted
+            self.predictions = neighbors.mode().values
+
+    def cross_validation(self, data, folds, label, evaluation):
+        scores = []
+        for i in range(5):
+            test = data.folds[i]
+            train = data.drop(folds[i].index)
+
+            train_label = train[label]
+            train_data = train.drop(label, axis=1)
+            test_label = test[label].to_numpy()
+            test = test.drop(label, axis=1)
+
+            self.calculate_knn(train_data, train_label, test)
+            scores.append(evaluation(test_label, self.predictions))
+        return scores
 
 
-def find_class(df, label):
-    """
-    :param df: A pandas DataFrame
-    :param label: The column name containing the correct classification
-    :return: a string, the most common class
-    """
-    classes = collections.Counter(df[label])
-    return classes.most_common(1)[0][0]
+class CondensedNearestNeighbor(KNearestNeighbor):
+    def __init__(self, train, test, target):
+        super().__init__(train, test, target)
 
-
-def condensed_nearest_neighbor(train, test, label, k, condensed=True):
-    #smallest subset z in instead of x such that errors don't increase
-    shuffled_df = train.sample(frac=1)
-    if condensed:
+    def calculate_knn(self, train, label, test):
+        shuffled_df = train.sample(frac=1)
         z = pd.DataFrame()
-    else:
-        z = pd.DataFrame(train)
-    count = 0
-    first = True
-    for index, row in shuffled_df.iterrows():
-        if first and condensed:
-            z = z.append(row)
-            first = False
-        else:
-            row_no_label = row.drop(label)
-            distance = euclidean_distance(z.drop(columns=label), row_no_label)
-            if z.loc[distance.nsmallest(1).index, label].values != row[label]:
-               z = z.append(row)
-               count = 0
+        for index, row in shuffled_df.iterrows():
+            if len(z) < self.k:
+                z = z.append(row)
             else:
-                count += 1
-    knn_predicted = k_nearest_neighbors(train, test, label, k)
-    return knn_predicted
+                distance = euclidean_distance(z.drop(columns=label), row.drop(label))
+                if self.sigma:
+                    distance = radial_basis_fn_gaussian_kernel(distance, self.sigma)
+                neighbor_classes = k_neighbors = np.argsort(distance, axis=0)[:self.k]
+                self.predict_class(label.to_list(), k_neighbors)
+                if neighbor_classes != row[label]:
+                    z = z.append(row)
+        return z
 
 
-def nearest_neighbor_cross_validation(df, folds, label, k_values=[1, 2, 3, 4, 5], eval_type="classification"):
-    """
+class EditedNearestNeighbor(KNearestNeighbor):
+    def __init__(self, train, test, target):
+        super().__init__(train, test, target)
 
-    :type k_values: list
-    """
-    knn_best_k = 1
-    knn_best_result = 0
-    cnn_best_k = 1
-    cnn_best_result = 0
-    for fold_number in range(len(folds)):
-        print(len(folds))
-        train = df.drop(folds[fold_number].index)
-        test = folds[fold_number]
-        knn_predicted = k_nearest_neighbors(train, test, label, k=k_values[fold_number])
-        cnn_predicted = condensed_nearest_neighbor(train, test, label, k_values[fold_number])
-        actual = test[label].to_list()
-        knn_results = eval_metric(actual, knn_predicted, eval_type)
-        cnn_results = eval_metric(actual, cnn_predicted, eval_type)
-        if knn_results > knn_best_result:
-            knn_best_k = k_values[fold_number]
-            knn_best_result = knn_results
-        if knn_results > cnn_best_result:
-            cnn_best_k = k_values[fold_number]
-            cnn_best_result = cnn_results
-
-    print(f"Best knn k-value: {knn_best_k}")
-    print(f"Best cnn k-value: {cnn_best_k}")
-    return knn_best_k, cnn_best_k
-
-
-def euclidean_distance(dp1, dp2):
-    distance = (dp1.subtract(dp2.values)).abs().sum(axis=1)
-    return distance
-
+    def calculate_knn(self, train, label, test):
+        shuffled_df = train.sample(frac=1)
+        z = pd.DataFrame(train)
+        for index, row in shuffled_df.iterrows():
+            if len(z) < self.k:
+                z = z.append(row)
+            else:
+                distance = euclidean_distance(z.drop(index).drop(columns=label), row.drop(label))
+                if self.sigma:
+                    distance = radial_basis_fn_gaussian_kernel(distance, self.sigma)
+                k_neighbors = np.argsort(distance, axis=0)[:self.k]
+                self.predict_class(label.to_list(), k_neighbors)
+                if k_neighbors != row[label]:
+                    z = z.drop(index)
+        return z
--- main.py
+++ main.py
@@ -1,110 +1,232 @@
-from processing import load_data, imputation, ordinal, nominal, standardization, discretization
-from learning import k_nearest_neighbors, k_fold, condensed_nearest_neighbor, nearest_neighbor_cross_validation
-from evaluation import majority_prediction, eval_metric
+from dataset import Data, ClassificationData
+from learning import KNearestNeighbor, CondensedNearestNeighbor, EditedNearestNeighbor
+from evaluation import cross_validation, tuning, classification, mse
 import numpy as np
+import time
 
-if __name__ == '__main__':
-    # classification files
 
-    # car data
+def load_car():
     car_file = 'data/raw/car.data'
-    car = load_data(car_file, names=['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class'])
+    car_data = ClassificationData('car', car_file, 'class', names=['buying', 'maint', 'doors',
+                                                              'persons', 'lug_boot', 'safety',
+                                                              'class'])
     car_column_order = {'buying': {'vhigh': 3, 'high': 2, 'med': 1, 'low': 0},
                         'maint': {'vhigh': 3, 'high': 2, 'med': 1, 'low': 0},
                         'doors': {'2': 0, '3': 1, '4': 2, '5more': 3},
                         'persons': {'2': 0, '4': 1, 'more': 3},
                         'lug_boot': {'small': 0, 'low': 0, 'med': 1, 'high': 2, 'big': 2},
                         'safety': {'small': 0, 'low': 0, 'med': 1, 'high': 2, 'big': 2}}
-    car = ordinal(car, car_column_order)
+    car_data.ordinal(car_column_order)
+    return car_data
 
-    # cancer data
-    #cancer_file = 'data/raw/breast-cancer-wisconsin.data'
-    #cancer = load_data(cancer_file, names=['Sample Code Number', 'Clump Thickness', 'Uniformity of Cell Size',
-    #                                        'Uniformity of Cell Shape', 'Marginal Adhesion',
-    #                                        'Single Epithelial Cell Size', 'Bare Nuclei', 'Bland Chromatin',
-    #                                        'Normal Nucleoli', 'Mitoses', 'class'],
-    #                    usecols=['Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape',
-    #                             'Marginal Adhesion', 'Single Epithelial Cell Size', 'Bare Nuclei', 'Bland Chromatin',
-    #                             'Normal Nucleoli', 'Mitoses', 'class'], na_values='?')
-    # cancer = imputation(cancer)
-    #
-    # # vote data
-    # vote_file = 'data/raw/house-votes-84.data'
-    # vote = load_data(vote_file, names=['class', 'handicapped-infants', 'water-project-cost-sharing',
-    #                                    'adoption-of-the-budget-resolution', 'physician-fee-freeze', 'el-salvador-aid',
-    #                                    'religious-groups-in-schools', 'anti-satellite-test-ban',
-    #                                    'aid-to-nicaraguan-contras', 'mx-missile', 'immigration',
-    #                                    'synfuels-corporation-cutback', 'education-spending', 'superfund-right-to-sue',
-    #                                    'crime', 'duty-free-exports', 'export-administration-act-south-africa'],
-    #                  na_values='?')
-    # vote = imputation(vote)
-    # vote = nominal(vote, [col for col in vote.columns if col != 'class'])
-    #
-    # # regression files
-    # abalone_file = 'data/raw/abalone.data'
-    # fire_file = 'data/raw/forestfires.data'
-    # machine_file = 'data/raw/machine.data'
-    #
-    # # load data
-    # abalone = load_data(abalone_file, names=['Sex', 'Length', 'Diameter', 'Height', 'Whole weight', 'Shucked weight',
-    #                                          'Viscera weight', 'Shell weight', 'Rings'])
-    # forest = load_data(fire_file, transform={'area': np.log}, usecols=['X', 'Y', 'FFMC', 'DMC', 'DC', 'ISI', 'temp',
-    #                                                                    'RH', 'wind', 'rain', 'area'])
-    # machine = load_data(machine_file, names=['vendor name', 'model', 'MYCT', 'MMIN', 'MMAX', 'CACH', 'CHMIN', 'CHMAX',
-    #                                          'PRP', 'ERP'],
-    #                     usecols=['MYCT', 'MMIN', 'MMAX', 'CACH', 'CHMIN', 'CHMAX', 'PRP'])
-    # abalone = nominal(abalone, ['Sex'])
-    # machine = discretization(machine, 'MYCT', 5, width=False)
 
-    print('Car Dataset:')
-    data = car
-    label = 'class'
-    eval_type = 'classification'
-    folds, validation = k_fold(data, 'class', validation=True)
-    validation_folds = k_fold(validation, 'class')[0]
-    validation_train = data.drop(validation_folds[0].index)
-    validation_test = validation_folds[0]
-    validation_true_labels = validation_test[label].to_list()
-    knn_results = {}
-    cnn_results = {}
-    #enn_results = {}
-    for i in range(1, 10):
-        knn_prediction = k_nearest_neighbors(validation_train, validation_test, label, k=i)
-        cnn_prediction = condensed_nearest_neighbor(validation_train, validation_test, label, k=i)
-        #enn_prediction
+def load_cancer():
+    cancer_file = 'data/raw/breast-cancer-wisconsin.data'
+    cancer_data = ClassificationData('cancer', cancer_file, 'class',
+                                names=['Sample Code Number', 'Clump Thickness',
+                                       'Uniformity of Cell Size', 'Uniformity of Cell Shape',
+                                       'Marginal Adhesion', 'Single Epithelial Cell Size',
+                                       'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli',
+                                       'Mitoses', 'class'],
+                                usecols=['Clump Thickness', 'Uniformity of Cell Size',
+                                         'Uniformity of Cell Shape', 'Marginal Adhesion',
+                                         'Single Epithelial Cell Size', 'Bare Nuclei',
+                                         'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'class'],
+                                na_values='?')
+    cancer_data.imputation()
+    return cancer_data
 
-        knn_eval = eval_metric(validation_true_labels, knn_prediction, eval_type)
-        cnn_eval = eval_metric(validation_true_labels, cnn_prediction, eval_type)
 
-        knn_results[i] = knn_eval
-        cnn_results[i] = cnn_eval
-        #enn_results[i] = enn_eval
+def load_vote():
+    vote_file = 'data/raw/house-votes-84.data'
+    vote_data = ClassificationData('vote', vote_file, 'class',
+                              names=['class', 'handicapped-infants', 'water-project-cost-sharing',
+                                     'adoption-of-the-budget-resolution', 'physician-fee-freeze',
+                                     'el-salvador-aid', 'religious-groups-in-schools',
+                                     'anti-satellite-test-ban', 'aid-to-nicaraguan-contras',
+                                     'mx-missile', 'immigration', 'synfuels-corporation-cutback',
+                                     'education-spending', 'superfund-right-to-sue', 'crime',
+                                     'duty-free-exports', 'export-administration-act-south-africa'],
+                              na_values='?')
+    vote_data.imputation()
+    vote_data.nominal('handicapped-infants')
+    vote_data.nominal('water-project-cost-sharing')
+    vote_data.nominal('adoption-of-the-budget-resolution')
+    vote_data.nominal('physician-fee-freeze')
+    vote_data.nominal('el-salvador-aid')
+    vote_data.nominal('religious-groups-in-schools')
+    vote_data.nominal('anti-satellite-test-ban')
+    vote_data.nominal('aid-to-nicaraguan-contras')
+    vote_data.nominal('mx-missile')
+    vote_data.nominal('immigration')
+    vote_data.nominal('synfuels-corporation-cutback')
+    vote_data.nominal('education-spending')
+    vote_data.nominal('superfund-right-to-sue')
+    vote_data.nominal('crime')
+    vote_data.nominal('duty-free-exports')
+    vote_data.nominal('export-administration-act-south-africa')
 
-    print(f"Best knn k-value: {max(knn_results, key=knn_results.get)}")
-    print(f"Best cnn k-value: {max(cnn_results, key=knn_results.get)}")
+    return vote_data
 
-   # knn_k, cnn_k = nearest_neighbor_cross_validation(data, validation_folds, 'class')
-    print('done')
-    results = {
-        'Fold #': [],
-        'Training Size': [],
-        'Test Size': [],
-        'Accuracy of Error': []
-    }
-    for i in range(knn_k):
-        train = data.drop(folds[i].index)
-        test = folds[i]
-        knn_predicted = k_nearest_neighbors(train, test, 'class', knn_k)
-        cnn_predicted = condensed_nearest_neighbor(train, test, 'class', cnn_k)
-        test_labels = test['class'].to_list()
-        knn_eval_results = eval_metric(test_labels, knn_predicted, 'classification')
-        cnn_eval_results = eval_metric(test_labels, cnn_predicted, 'classification')
 
-        results['Fold #'].append(i)
-        results['Training Size'].append(len(train))
-        results['Test Size'].append(len(test))
-        results['Accuracy of Error'].append(knn_eval_results)
+def load_abalone():
+    abalone_file = 'data/raw/abalone.data'
+    abalone_data = Data('abalone', abalone_file, 'Rings', names=['Sex', 'Length', 'Diameter', 'Height',
+                                                            'Whole weight', 'Shuck ed weight',
+                                                            'Viscera weight', 'Shell weight',
+                                                            'Rings'])
+    abalone_data.nominal('Sex')
 
-    print()
-    print(f'The Best Eval Score: {max(results["Accuracy of Error"])}')
-    print(f'The Average of All Eval Scores: {sum(results["Accuracy of Error"]) / len(results["Accuracy of Error"])}')
+    return abalone_data
+
+
+def load_forest_fire():
+    fire_file = 'data/raw/forestfires.data'
+    fire_data = Data('forest', fire_file, 'area', transform={'area': np.log},
+                     usecols=['X', 'Y', 'FFMC', 'DMC', 'DC', 'ISI', 'temp', 'RH', 'wind', 'rain',
+                              'area'])
+    return fire_data
+
+
+def load_machine():
+    machine_file = 'data/raw/machine.data'
+    machine_data = Data('machine', machine_file, 'PRP', names=['vendor name', 'model', 'MYCT', 'MMIN',
+                                                          'MMAX', 'CACH', 'CHMIN', 'CHMAX', 'PRP',
+                                                          'ERP'],
+                   usecols=['MYCT', 'MMIN', 'MMAX', 'CACH', 'CHMIN', 'CHMAX', 'PRP'])
+
+    machine_data.normalization(['MYCT', 'MMIN', 'MMAX', 'CACH', 'CHMIN', 'CHMAX', 'PRP'])
+    #machine_data.discretization('MYCT', 5, width=False)
+
+    return machine_data
+
+
+if __name__ == '__main__':
+    car = load_car()
+    cancer = load_cancer()
+    vote = load_vote()
+
+    abalone = load_abalone()
+    forest = load_forest_fire()
+    machine = load_machine()
+
+    for data in [car, cancer, vote]:
+        knn = KNearestNeighbor(data.label, k=1)
+        best_score, best_k = tuning(data, knn, classification)
+        print(f'Name: {data.name}')
+        print(f'Best Fold: {best_k}')
+
+        scores = cross_validation(knn, data.df, data.folds, data.label, classification)
+        print(f'Best Fold: {np.argmax(scores)}, Best Score: {max(scores)}')
+        print()
+
+    for data in [abalone, forest, machine]:
+        knn = KNearestNeighbor(data.label, k=1)
+        best_score = np.inf
+        best_sigma = None
+        best_k = None
+        for sigma in [1, 0.8, 0.5, 0.3, 0.15, 0.1, 0.005, 0.001, 0.0008, 0.0005, 0.0003]:
+            score, k = tuning(data, knn, mse, sigma)
+            if score < best_score:
+                best_score = score
+                best_sigma = sigma
+                best_k = k
+        data.sigma = best_sigma
+        data.k = best_k
+        print(f'Name: {data.name}')
+        print(f'Best k-value: {best_k}, Best Sigma: {best_sigma}')
+
+        scores = cross_validation(knn, data.df, data.folds, data.label, mse)
+        print(f'Best Fold: {np.argmin(scores)}, Best Score: {min(scores)}')
+        print()
+
+#    print(knn_results)
+#    print(cnn_results)
+#    print(enn_results)
+#    print(f"Best knn k-value, epsilon, sigma: {knn_k, knn_epsilon, knn_sigma}")
+#    print(f"Best cnn k-value epsilon, sigma: {cnn_k, cnn_epsilon, cnn_sigma}")
+#    print(f"Best enn k-value epsilon, sigma: {enn_k, enn_epsilon, enn_sigma}")
+#
+#     knn_results = {
+#         'Fold #': [],
+#         'Training Size': [],
+#         'Test Size': [],
+#         'Accuracy of Error': []
+#     }
+#     cnn_results = {
+#         'Fold #': [],
+#         'Training Size': [],
+#         'Test Size': [],
+#         'Accuracy of Error': []
+#     }
+#
+#     enn_results = {
+#         'Fold #': [],
+#         'Training Size': [],
+#         'Test Size': [],
+#         'Accuracy of Error': []
+#     }
+#     knn_k = 11
+#     cnn_k = 9
+#     enn_k = 19
+#     cnn_train = condensed_edited_nearest_neighbor(tuning_train, label, k=3)
+#     enn_train = condensed_edited_nearest_neighbor(tuning_train, label, k=1, condensed=False)
+#
+#     for i in range(len(folds)):
+#         train = data.drop(folds[i].index)
+#         test = folds[i]
+#         start = timer()
+#         knn_predicted = k_nearest_neighbors(train, test, label, classification, knn_k)
+#         end = timer()
+#         print(f'knn time: {end - start}')
+#         start = timer()
+#         cnn_predict = k_nearest_neighbors(cnn_train, test, label, classification, cnn_k)
+#         end = timer()
+#         print(f'cnn time: {end - start}')
+#         start = timer()
+#         enn_predict = k_nearest_neighbors(enn_train, test, label, classification, enn_k)
+#         end = timer()
+#         print(f'enn time: {end - start}')
+#         test_labels = test[label].to_list()
+#         knn_eval_results = eval_metric(test_labels, knn_predicted, eval_type)
+#         cnn_eval_results = eval_metric(test_labels, cnn_predict, eval_type)
+#         enn_eval_results = eval_metric(test_labels, enn_predict, eval_type)
+#
+#         knn_results['Fold #'].append(i)
+#         knn_results['Training Size'].append(len(train))
+#         knn_results['Test Size'].append(len(test))
+#         knn_results['Accuracy of Error'].append(knn_eval_results)
+#
+#         cnn_results['Fold #'].append(i)
+#         cnn_results['Training Size'].append(len(train))
+#         cnn_results['Test Size'].append(len(test))
+#         cnn_results['Accuracy of Error'].append(cnn_eval_results)
+#
+#         enn_results['Fold #'].append(i)
+#         enn_results['Training Size'].append(len(train))
+#         enn_results['Test Size'].append(len(test))
+#         enn_results['Accuracy of Error'].append(enn_eval_results)
+#
+#     print()
+#     print(f'knn_results: {knn_results}')
+#     print(f'cnn_results: {cnn_results}')
+#     print(f'enn_results: {enn_results}')
+#     print(f"Best knn fold: {max(knn_results, key=knn_results.get)}")
+#     print(f"Best cnn fold: {max(cnn_results, key=cnn_results.get)}")
+#     print(f"Best enn fold: {max(enn_results, key=enn_results.get)}")
+#
+#     print()
+#     print('knn:')
+#     print(f'The Best Eval Score: {max(knn_results["Accuracy of Error"])}')
+#     print(
+#         f'The Average of All Eval Scores: {sum(knn_results["Accuracy of Error"]) / len(knn_results["Accuracy of Error"])}')
+#     print()
+#     print('cnn:')
+#     print(f'The Best Eval Score: {max(cnn_results["Accuracy of Error"])}')
+#     print(
+#         f'The Average of All Eval Scores: {sum(cnn_results["Accuracy of Error"]) / len(cnn_results["Accuracy of Error"])}')
+#     print()
+#     print('enn:')
+#     print(f'The Best Eval Score: {max(enn_results["Accuracy of Error"])}')
+#     print(
+#         f'The Average of All Eval Scores: {sum(enn_results["Accuracy of Error"]) / len(enn_results["Accuracy of Error"])}')
